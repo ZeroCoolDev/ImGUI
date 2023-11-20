78fb534fd0a36e5a11f7844a736b9550baecf8ba
cnpatton <cooper.patton.zcs@gmail.com>
2023-11-19 21:32:21 -0800

    testing attaching ImGUI to a running actor

--- ZC_ImGUI/Source/ZC_ImGUI/ZC_ImGUI.Build.cs
+++ ZC_ImGUI/Source/ZC_ImGUI/ZC_ImGUI.Build.cs
@@ -8,7 +8,7 @@ public class ZC_ImGUI : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "ImGui" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
